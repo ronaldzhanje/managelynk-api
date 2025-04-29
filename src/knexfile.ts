e95d46faaf47a855fbf8ed3721338d5b13d24ff1
Ronald Zhanje <ronaldzhanje@users.noreply.github.com>
2025-04-28 22:11:43 -0600

    added pool

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -16,6 +16,10 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'lynkdb',
     port: parseInt(process.env.DB_PORT || '5433'),
+    pool: {
+      min: 2,
+      max: 20
+    },
     ...sslConfig
   },
   migrations: {
